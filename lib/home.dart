3bfb468c109ca68d4d18042ef2d6c4dda08497dc
Akshay Patole <akshay.patole@thehirehq.com>
2025-05-08 14:58:21 +0530

    first commit

--- lib/home.dart
+++ lib/home.dart
@@ -11,11 +11,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("QR Scanner"),),
-      body: const Column(
-        children: [
-          Icon(Icons.camera_alt_outlined)
-        ],
+      appBar: AppBar(
+        title: const Text("QR Scanner"),
+      ),
+      body: const Center(
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.fromBorderSide(
+              BorderSide(color: Colors.black, width: 2),
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Icon(
+              Icons.camera_alt_outlined,
+              size: 48,
+              color: Colors.black,
+            ),
+          ),
+        ),
       ),
     );
   }
