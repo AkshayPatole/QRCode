6043f24e31b667f534e2cf1c49cc528ee243125c
Akshay Patole <akshay.patole@thehirehq.com>
2025-05-08 15:15:17 +0530

    first commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+import 'custom/icon_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,24 +18,85 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text("QR Scanner"),
       ),
-      body: const Center(
+      body: Center(
         child: DecoratedBox(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             border: Border.fromBorderSide(
               BorderSide(color: Colors.black, width: 2),
             ),
             borderRadius: BorderRadius.all(Radius.circular(12)),
           ),
           child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Icon(
-              Icons.camera_alt_outlined,
-              size: 48,
-              color: Colors.black,
+            padding: const EdgeInsets.all(16.0),
+            child: InkWell(
+              onTap: () {
+                _openCameraWidget(context);
+                print(
+                      "**********************Open Camera***********************");
+              },
+              child: const IconWidget(iconData: Icons.qr_code_scanner),
             ),
           ),
         ),
       ),
     );
+  }
+
+  _openCameraWidget(BuildContext context) async {
+    var status = await Permission.camera.status;
+    if (status.isDenied) {
+      await Permission.camera.request();
+      status = await Permission.camera.status;
+    }
+
+    if (status.isGranted) {
+      try {
+        String barcode = await FlutterBarcodeScanner.scanBarcode(
+            "#0000FF", "Cancel", true, ScanMode.DEFAULT);
+
+        if (barcode != "-1") {
+          // ignore: use_build_context_synchronously
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Scanned QR Code'),
+                content: Text(barcode),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      } catch (e) {
+        print('Error: $e');
+      }
+    } else {
+      // ignore: use_build_context_synchronously
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Permission Required'),
+            content:
+                const Text('Camera permission is required to scan QR codes.'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
